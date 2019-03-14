d9afa06500665212d4ce1eebf3a432895b5c844d
huangqiang <zhuoluo.hq@gmail.com>
2019-03-14 10:27:17 +0800

    调整分工

--- packages/web-framework/src/utils/index.js
+++ packages/web-framework/src/utils/index.js
@@ -1,9 +1,7 @@
 export { default as apiService } from './api_service';
-export { default as stringFormat } from './string_format';
 export { default as generateReducer } from './generate_reducer';
 export { default as generateRouteContent } from './generate_route_content';
 
 export * from './api_service';
 export * from './obj_convert';
-export * from './dom_position';
 export * from './qs';
