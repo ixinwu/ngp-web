a927f45af321fa59964394ac4458648ceb89e8c1
huangqiang <zhuoluo.hq@gmail.com>
2019-07-25 16:33:34 +0800

    更新核心connect

--- README.md
+++ README.md
@@ -6,9 +6,9 @@
 
 | Package | 描述 | 版本 |
 | ------ | ------ | ------ |
-| [@ixinwu-ngp/web-framework](./packages/web-framework/README.md) | 前端框架 | v1.2.12
+| [@ixinwu-ngp/web-framework](./packages/web-framework/README.md) | 前端框架 | v1.2.13
 | [@ixinwu-ngp/generator-ngp-spa](./packages/generator-ngp-spa/README.md) | 前端脚手架 | v1.1.3
-| [@ixinwu-ngp/web-core](./packages/web-core/README.md) | 前端核心 | v1.1.4
+| [@ixinwu-ngp/web-core](./packages/web-core/README.md) | 前端核心 | v1.1.5
 | [@ixinwu-ngp/materials-component](./packages/materials-component/README.md) | 组件库包括样式解决方案 | v1.1.3
 | [@ixinwu-ngp/materials-block](./packages/materials-block/README.md) | 功能区块库 | v1.1.6
 | [@ixinwu-ngp/materials-layout](./packages/materials-layout/README.md) | 布局库 | 规划阶段
--- packages/web-core/src/connect.js
+++ packages/web-core/src/connect.js
@@ -1,151 +1,35 @@
-import { connectAdvanced } from 'react-redux';
-import defaultMapDispatchToPropsFactories from './react-dedux/connect/mapDispatchToProps';
-import defaultMapStateToPropsFactories from './react-dedux/connect/mapStateToProps';
-import defaultMergePropsFactories from './react-dedux/connect/mergeProps';
-import shallowEqual from './react-dedux/utils/shallowEqual';
-import defaultSelectorFactory from './selector_factory';
+import { connect } from 'react-redux';
+import core from './core';
 
-/*
-  connect is a facade over connectAdvanced. It turns its args into a compatible
-  selectorFactory, which has the signature:
+function ngpConnect(mapStateToProps, mapDispatchToProps, mergeProps, options) {
+  let mergePropsWrapper;
 
-    (dispatch, options) => (nextState, nextOwnProps) => nextFinalProps
-
-  connect passes its args to connectAdvanced as options, which will in turn pass them to
-  selectorFactory each time a Connect component instance is instantiated or hot reloaded.
-
-  selectorFactory returns a final props selector from its mapStateToProps,
-  mapStateToPropsFactories, mapDispatchToProps, mapDispatchToPropsFactories, mergeProps,
-  mergePropsFactories, and pure args.
-
-  The resulting final props selector is called by the Connect component instance whenever
-  it receives new props or store state.
- */
-
-function match(arg, factories, name) {
-  for (let i = factories.length - 1; i >= 0; i -= 1) {
-    const result = factories[i](arg);
-    if (result) return result;
+  if (typeof mergeProps === 'function') {
+    mergePropsWrapper = (stateProps, dispatchProps, ownProps) => {
+      const mergedProps = mergeProps(stateProps, dispatchProps, ownProps);
+      // 保存identity对应的props
+      if (ownProps && ownProps.identity) {
+        core.identityProps[ownProps.identity] = mergedProps;
+      }
+      return mergedProps;
+    };
+  } else {
+    mergePropsWrapper = (stateProps, dispatchProps, ownProps) => {
+      const mergedProps = { ...ownProps, ...stateProps, ...dispatchProps };
+      // 保存identity对应的props
+      if (ownProps && ownProps.identity) {
+        core.identityProps[ownProps.identity] = mergedProps;
+      }
+      return mergedProps;
+    };
   }
 
-  return (dispatch, options) => {
-    throw new Error(
-      `Invalid value of type ${typeof arg} for ${name} argument when connecting component ${
-        options.wrappedComponentName
-      }.`,
-    );
-  };
-}
-
-function strictEqual(a, b) {
-  return a === b;
-}
-
-// createConnect with default args builds the 'official' connect behavior. Calling it with
-// different options opens up some testing and extensibility scenarios
-export function createConnect({
-  connectHOC = connectAdvanced,
-  mapStateToPropsFactories = defaultMapStateToPropsFactories,
-  mapDispatchToPropsFactories = defaultMapDispatchToPropsFactories,
-  mergePropsFactories = defaultMergePropsFactories,
-  selectorFactory = defaultSelectorFactory,
-} = {}) {
-  return function connect(
+  return connect(
     mapStateToProps,
     mapDispatchToProps,
-    mergeProps,
-    {
-      pure = true,
-      areStatesEqual = strictEqual,
-      areOwnPropsEqual = shallowEqual,
-      areStatePropsEqual = shallowEqual,
-      areMergedPropsEqual = shallowEqual,
-      ...extraOptions
-    } = {},
-  ) {
-    const initMapStateToProps = match(mapStateToProps, mapStateToPropsFactories, 'mapStateToProps');
-    const initMapDispatchToProps = match(
-      mapDispatchToProps,
-      mapDispatchToPropsFactories,
-      'mapDispatchToProps',
-    );
-    const initMergeProps = match(mergeProps, mergePropsFactories, 'mergeProps');
-
-    return connectHOC(selectorFactory, {
-      // used in error messages
-      methodName: 'nephalemConnect',
-
-      // used to compute Connect's displayName from the wrapped component's displayName.
-      getDisplayName: name => `NephalemConnect(${name})`,
-
-      // if mapStateToProps is falsy, the Connect component doesn't subscribe to store state changes
-      shouldHandleStateChanges: Boolean(mapStateToProps),
-
-      // passed through to selectorFactory
-      initMapStateToProps,
-      initMapDispatchToProps,
-      initMergeProps,
-      pure,
-      areStatesEqual,
-      areOwnPropsEqual,
-      areStatePropsEqual,
-      areMergedPropsEqual,
-
-      // any extra options args can override defaults of connect or connectAdvanced
-      ...extraOptions,
-    });
-  };
+    mergePropsWrapper,
+    options,
+  );
 }
 
-export default createConnect();
-
-// import React, { Component } from 'react';
-// import { connect as reduxConnect } from 'react-redux';
-
-// const getDisplayName = name => `ConnectAdvanced(${name})`;
-
-// function ppHOCFactory({ identity, ...handleTriggers }) {
-//   return function ppHOC(WrappedComponent) {
-//     const wrappedComponentName =
-//       WrappedComponent.displayName || WrappedComponent.name || 'Component';
-
-//     const displayName = getDisplayName(wrappedComponentName);
-
-//     // eslint-disable-next-line
-//     class PP extends Component {
-//       render() {
-//         const newProps = {
-//           identity,
-//         };
-
-//         Object.keys(handleTriggers).forEach(key => {
-//           newProps[key] = (...args) => {
-//             return handleTriggers[key](identity, this.props, ...args);
-//           };
-//         });
-
-//         return <WrappedComponent {...this.props} {...newProps} />;
-//       }
-//     }
-
-//     PP.WrappedComponent = WrappedComponent;
-//     PP.displayName = displayName;
-
-//     return PP;
-//   };
-// }
-
-// export default function connect(...args) {
-//   return Comp => props => {
-//     const ConnectComp = reduxConnect(...args)(Comp);
-//     return ppHOCFactory(props)(ConnectComp);
-//     // const PPComp = ppHOCFactory(props)(Comp);
-//     // return reduxConnect(...args)(PPComp);
-//   };
-// }
-
-// export default function connect(config) {
-//   return Comp => {
-//     return ppHOCFactory(config)(Comp);
-//   };
-// }
+export default ngpConnect;
