e7f90a5ee639fda8cfa214ef444d9167279f0a63
huangqiang <zhuoluo.hq@gmail.com>
2019-08-06 13:39:53 +0800

    test bpmn

--- templates/ngp-spa/src/pages/test/view.js
+++ templates/ngp-spa/src/pages/test/view.js
@@ -2,13 +2,32 @@ import React, { Component } from 'react';
 import { connect } from '@ixinwu-ngp/web-core';
 import { withStyles } from '@ixinwu-ngp/web-styles';
 import styles from './styles';
+import { FlowDesign } from 'react-flow-design';
 
 class XXXX extends Component {
+  state = {
+    processesDesign: [],
+  };
+
   componentDidMount() {}
+
+  handleFlowDesignChange = design => {
+    this.setState(prevState => ({
+      ...prevState,
+      processesDesign: design,
+    }));
+  };
 
   render() {
     const { classes, aaa, bbb } = this.props;
-    return <div className={classes.container}>test_b, {aaa}, {bbb}</div>;
+    return (
+      <div className={classes.container}>
+        test_b, {aaa}, {bbb}
+        <div>
+          <FlowDesign design={this.state.processesDesign} onChange={this.handleFlowDesignChange} />
+        </div>
+      </div>
+    );
   }
 }
 
