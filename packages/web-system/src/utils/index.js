b3bb5b92be527609219456c644493db6b4b2230b
huangqiang <zhuoluo.hq@gmail.com>
2019-08-27 15:21:14 +0800

    v1.1.9

--- packages/web-system/src/utils/index.js
+++ packages/web-system/src/utils/index.js
@@ -1,7 +1,5 @@
-export { default as apiService } from './api_service';
 export { default as generateReducer } from './generate_reducer';
 export { default as generateRouteContent } from './generate_route_content';
 
-export * from './api_service';
 export * from './obj_convert';
 export * from './qs';
